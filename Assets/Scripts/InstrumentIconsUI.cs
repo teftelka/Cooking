81cadda3256b4266f1d092a210c019082b86a0d4
Karina <44575378+teftelka@users.noreply.github.com>
2026-04-26 20:34:05 +0300

    chg: instrument icons refactor

--- Assets/Scripts/InstrumentIconsUI.cs
+++ Assets/Scripts/InstrumentIconsUI.cs
@@ -5,9 +5,6 @@ public class InstrumentIconsUI : MonoBehaviour
 {
     [SerializeField] private CookingTool cookingTool;
     [SerializeField] private Transform iconTemplate;
-    [SerializeField] private Sprite defaultIcon;
-    
-    private int iconsCount;
     
     private void Start()
     {
@@ -21,29 +18,24 @@ public class InstrumentIconsUI : MonoBehaviour
         {
             if (child == iconTemplate)
             {
-                child.GetComponent<IconsTemplateUI>().SetIcon(defaultIcon);
+                iconTemplate.gameObject.SetActive(true);
                 continue;
             }
             Destroy(child.gameObject);
         }
-        iconsCount = 0;
     }
 
     private void HandleProductAdded(object sender, CookingTool.OnProductAddedEventArgs e)
     {
+        iconTemplate.gameObject.SetActive(false);
         var sprite = e.product.GetSpriteRenderer().sprite;
-        iconsCount++;
         UpdateVisual(sprite);
     }
 
     private void UpdateVisual(Sprite icon)
     {
-        if (iconsCount == 1)
-        {
-            iconTemplate.GetComponent<IconsTemplateUI>().SetIcon(icon);
-            return;
-        }
         Transform newIcon = Instantiate(iconTemplate, transform);
         newIcon.GetComponent<IconsTemplateUI>().SetIcon(icon);
+        newIcon.gameObject.SetActive(true);
     }
 }
