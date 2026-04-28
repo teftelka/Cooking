e2295f80378082d25e0e903014418287d3c14a7b
Karina <44575378+teftelka@users.noreply.github.com>
2026-04-28 18:19:25 +0300

    add: scores update

---
+++ Assets/Scripts/UIScripts/ScoreManager.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+
+namespace UIScripts
+{
+    public class ScoreManager: MonoBehaviour
+    {
+        public static ScoreManager Instance { get; set; }
+        
+        [SerializeField] private int currentMoney;
+        [SerializeField] private int currentExperience;
+
+        public EventHandler OnExperienceUpdated;
+        public EventHandler OnMoneyUpdated;
+        
+        
+        private void Awake()
+        {
+            Instance = this;
+        }
+        
+        private void Start()
+        {
+            OrderManager.Instance.OnOrderComplete += HandleOrderComplete;
+        }
+
+        private void HandleOrderComplete(object sender, OrderManager.OnOrderCompleteEventArgs e)
+        {
+            ExperienceAmountUpdate(555);
+            MoneyAmountUpdate(100);
+
+        }
+
+        private void ExperienceAmountUpdate(int exp)
+        {
+            currentExperience += exp;
+            OnExperienceUpdated?.Invoke(this, EventArgs.Empty);
+        }
+        
+        private void MoneyAmountUpdate(int money)
+        {
+            currentMoney += money;
+            OnMoneyUpdated?.Invoke(this, EventArgs.Empty);
+        }
+
+
+        public int GetExpAmount()
+        {
+            return currentExperience;
+        }
+        
+        public int GetMoneyAmount()
+        {
+            return currentMoney;
+        }
+    }
+}
--- Assets/Scripts/UIScripts/ScoreManagerUI.cs
+++ Assets/Scripts/UIScripts/ScoreManagerUI.cs
@@ -1,16 +1,28 @@
+using System;
+using System.Data;
+using TMPro;
+using UIScripts;
 using UnityEngine;
 
 public class ScoreManagerUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    [SerializeField] private TextMeshProUGUI experienceText;
+    [SerializeField] private TextMeshProUGUI moneyText;
+
+    private void Start()
     {
-        
+        ScoreManager.Instance.OnExperienceUpdated += OnExperienceUpdated;
+        ScoreManager.Instance.OnMoneyUpdated += OnMoneyUpdated;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMoneyUpdated(object sender, EventArgs e)
     {
-        
+        moneyText.text = ScoreManager.Instance.GetMoneyAmount().ToString();
+    }
+
+    private void OnExperienceUpdated(object sender, EventArgs e)
+    {
+        experienceText.text = ScoreManager.Instance.GetExpAmount().ToString();
     }
 }
